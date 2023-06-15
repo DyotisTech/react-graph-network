a188fa199295da53a6fc584e4d34475b3c11e41d
DyotisianAnurag <anurag@dyotis.com>
2023-06-15 13:43:59 +0530

    addZoom function removed

--- interactions.js
+++ interactions.js
@@ -12,74 +12,23 @@ var _d3Selection = require("d3-selection");
 var _d3Zoom = require("d3-zoom");
 
 var _events = require("./events");
-var addZoom = function addZoom(svg, zoomDepth) {
-  if (zoomDepth) {
-    var svgHeight = 200;
-    var svgWidth = 200;
-    var centerX = svgWidth / 2;
-    var centerY = svgHeight / 2;
-
-    var zoomed = function () {
-      var transform = _d3Selection.event.transform;
-      var scale = transform.k; // Get the scaling factor
-
-      // Apply scaling only
-      svg.selectAll("._graphZoom")
-        .attr("transform", "translate(" + centerX + "," + centerY + ") scale(" + scale + ")");
-    };
-
-    var zoom = _d3Zoom.zoom()
-      .extent([
-        [0, 0],
-        [svgWidth, svgHeight],
-      ])
-      .scaleExtent([1, zoomDepth])
-      .on("zoom", zoomed);
-
-    var drag = _d3Drag.drag()
-      .on("start", function () {
-        if (_d3Selection.event.sourceEvent.type !== "brush") {
-          _d3Selection.event.sourceEvent.stopPropagation();
-        }
-      })
-      .on("drag", function () {
-        if (_d3Selection.event.sourceEvent.type !== "brush") {
-          svg.attr("transform", _d3Selection.event.transform);
-        }
-      });
-
-    var zoomIn = function () {
-      zoom.scaleBy(svg.transition().duration(500), 1.2);
-    };
-
-    var zoomOut = function () {
-      zoom.scaleBy(svg.transition().duration(500), 0.8);
-    };
-
-    // Bind zoom in and zoom out functions to UI buttons
-    _d3Selection.select("#zoom-in-button").on("click", zoomIn);
-    _d3Selection.select("#zoom-out-button").on("click", zoomOut);
-
-    svg.call(zoom).call(drag);
-  }
-
-  return svg;
-};
-
-exports.addZoom = addZoom;
-
 // var addZoom = function addZoom(svg, zoomDepth) {
 //   if (zoomDepth) {
-//     var svgHeight = svg._groups[0][0].clientHeight;
-//     var svgWidth = svg._groups[0][0].clientWidth;
+//     var svgHeight = 200;
+//     var svgWidth = 200;
+//     var centerX = svgWidth / 2;
+//     var centerY = svgHeight / 2;
 
-//     var zoomed = function zoomed() {
-//       svg
-//         .selectAll("._graphZoom")
-//         .attr("transform", _d3Selection.event.transform);
+//     var zoomed = function () {
+//       var transform = _d3Selection.event.transform;
+//       var scale = transform.k; // Get the scaling factor
+
+//       // Apply scaling only
+//       svg.selectAll("._graphZoom")
+//         .attr("transform", "translate(" + centerX + "," + centerY + ") scale(" + scale + ")");
 //     };
 
-//     var zoom = (0, _d3Zoom.zoom)()
+//     var zoom = _d3Zoom.zoom()
 //       .extent([
 //         [0, 0],
 //         [svgWidth, svgHeight],
@@ -87,7 +36,7 @@ exports.addZoom = addZoom;
 //       .scaleExtent([1, zoomDepth])
 //       .on("zoom", zoomed);
 
-//     var drag = (0, _d3Drag.drag)()
+//     var drag = _d3Drag.drag()
 //       .on("start", function () {
 //         if (_d3Selection.event.sourceEvent.type !== "brush") {
 //           _d3Selection.event.sourceEvent.stopPropagation();
@@ -118,6 +67,57 @@ exports.addZoom = addZoom;
 // };
 
 // exports.addZoom = addZoom;
+
+var addZoom = function addZoom(svg, zoomDepth) {
+  if (zoomDepth) {
+    var svgHeight = 0;
+    var svgWidth = 0;
+
+    var zoomed = function zoomed() {
+      svg
+        .selectAll("._graphZoom")
+        .attr("transform", _d3Selection.event.transform);
+    };
+
+    var zoom = (0, _d3Zoom.zoom)()
+      .extent([
+        [0, 0],
+        [svgWidth, svgHeight],
+      ])
+      .scaleExtent([1, zoomDepth])
+      .on("zoom", zoomed);
+
+    var drag = (0, _d3Drag.drag)()
+      .on("start", function () {
+        if (_d3Selection.event.sourceEvent.type !== "brush") {
+          _d3Selection.event.sourceEvent.stopPropagation();
+        }
+      })
+      .on("drag", function () {
+        if (_d3Selection.event.sourceEvent.type !== "brush") {
+          svg.attr("transform", _d3Selection.event.transform);
+        }
+      });
+
+    var zoomIn = function () {
+      zoom.scaleBy(svg.transition().duration(500), 1.2);
+    };
+
+    var zoomOut = function () {
+      zoom.scaleBy(svg.transition().duration(500), 0.8);
+    };
+
+    // Bind zoom in and zoom out functions to UI buttons
+    _d3Selection.select("#zoom-in-button").on("click", zoomIn);
+    _d3Selection.select("#zoom-out-button").on("click", zoomOut);
+
+    svg.call(zoom).call(drag);
+  }
+
+  return svg;
+};
+
+exports.addZoom = addZoom;
 
 var addHoverOpacity = function addHoverOpacity(node, link, hoverOpacity) {
   node
